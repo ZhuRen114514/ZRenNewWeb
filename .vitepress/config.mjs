72e322e0e902473cd23ed6fe14b5d719a0e7ba0a
主人 <zrentech@zren.top>
2024-09-06 11:57:21 +0800

    新的开始

---
+++ .vitepress/config.mjs
@@ -0,0 +1,48 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  head: [["link", { rel: "icon", href: "/logo.png" }]],
+  title: "ZRenTech",
+  description: "欢迎你的到来",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    logo: '/logo.png',
+
+    nav: [
+      {text:"首页", link:"/"},
+      { text: '旧版主页', items: [
+        {text: "主人科技辅助售卖页", link: "https://www.zren.top/main/index.html"},
+        {text: "主人科技原版主页", link: "https://www.zren.top"}
+      ]},
+      { text: '使用教程', items: [
+        {text: "示例", link: "/markdown-examples.md"},
+        {text: "API 示例", link: "/api-examples"}
+      ]}
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/ZhuRen114514' },
+      { icon: 'bilibili', link: 'https://github.com/vuejs/vitepress' }
+    ],
+
+    footer: {
+      copyright: "© 2024 ZRenTech 主人科技"
+    }
+  }
+})
+
+
+
+
+
---
+++ index.md
@@ -0,0 +1,36 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "ZRenTech"
+  text: "欢迎你的到来"
+  tagline: "主人总是尝试学习各种东西但是又记不住..."
+  image: 
+    src: "/logo.png"
+    alt: "logo"
+    style: "max-width: 150px; height: auto; border-radius: 40px; opacity: 0.8;" # 设置样式
+  actions:
+    - theme: brand
+      text: 项目列表
+      link: /markdown-examples
+    - theme: alt
+      text: 介绍
+      link: /api-examples
+
+features:
+  - title: 网盘
+    details: 存放着主人制作的辅助(貌似还没有公开这个网盘)
+    link: /
+  - title: 卡网
+    details: 可以在这里购买到卡密(暂时还没有可付费内容供您选择)
+    link: /
+
+
+
+
+
+
+
+---
+
